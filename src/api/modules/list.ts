88870525da9930c78e616dbad6f7fe0fb6f8aec9
Dmitry Vorobiev <dvorobiev@itoolabs.com>
2023-03-27 17:31:47 +0300

    redux added

--- src/api/modules/list.ts
+++
@@ -1,6 +0,0 @@
-import {http} from "../";
-import {GitHubListItem, ListRequestParams} from "../../definitions/list";
-
-export async function get(params: ListRequestParams) {
-    return await http.get<GitHubListItem[]>("events", params)
-}
