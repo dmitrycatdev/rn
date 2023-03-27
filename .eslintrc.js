0713e1aae1c616a373a15889565f58e10a7d1ae3
Dmitry Vorobiev <dvorobiev@itoolabs.com>
2023-03-27 18:50:11 +0300

    eslint + prettier

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+// TODO
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'airbnb',
+        'prettier',
+    ],
+    overrides: [],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['react', 'prettier', '@typescript-eslint'],
+    rules: { 'prettier/prettier': 'error' },
+}
